a1057e3addffb278503f2ab0dd8ed709c649e1ad
gracesuhyun <gracesuhyun@gmail.com>
2023-01-23 05:10:21 -0500

    fix menu bar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,40 +2,39 @@ import React from 'react'
 import './header.css';
 import '../../index.css';
 
+
+
+const Header = () => {
+
   /*=============== SHOW MENU ===============*/
   const navMenu = document.getElementById('nav-menu'),
-        navToggle = document.getElementById('nav-toggle'),
-        navClose = document.getElementById('nav-close')
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close')
 
   /*===== MENU SHOW =====*/
   /* Validate if constant exists */
   if(navToggle){
-    navToggle.addEventListener('click', () =>{
-      navMenu.classList.add('show-menu')
-    })
+  navToggle.addEventListener('click', () =>{
+    navMenu.classList.add('show-menu')
+  })
   }
 
   /*===== MENU HIDDEN =====*/
   /* Validate if constant exists */
   if(navClose){
-    navClose.addEventListener('click', () =>{
-      navMenu.classList.remove('show-menu')
-    })
+  navClose.addEventListener('click', () =>{
+    navMenu.classList.remove('show-menu')
+  })
   }
 
   /*=============== REMOVE MENU MOBILE ===============*/
   const navLink = document.querySelectorAll('.nav__link')
 
   const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show-menu')
   }
   navLink.forEach(n => n.addEventListener('click', linkAction))
-
-
-
-
-const Header = () => {
 
   return (
     <header className="header" id="header">
